40d5e1d7e7b3b25e529c91c9a9dc2a359947a1cf
nnorthcott <34338972@student.murdoch.edu.au>
2024-03-16 15:25:15 +0800

    Homework W3

---
+++ footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+            </div>
+            <div class="col-md-6">
+                <p>Terms and Conditions</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer();?>
+</body>
+</html>

3357eef01f2c9bda75ab46e012077445dcd2692f
nnorthcott <34338972@student.murdoch.edu.au>
2024-03-23 09:21:31 +0800

    Index Update 2

--- index.php
+++ index.php
@@ -6,17 +6,6 @@
         </div>
     </header>
     <?php get_header();?>   <!--  Tells WordPress to include header.php -->
-         <section class="container-fluid aboutbg text-center">
-            <div class="container">
-                <h2> GAMES &amp; LEVEL DESIGNER </h2>
-                <p class="particle">  and particle wizard</p>
-                <p class="about-text"> My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
-            </div><!-- container-->
-            <div class="blocker"></div>
-        </section>
-    
-    
- 
     <section class="container-fluid articlesbg">
             <div class="container">
                 <h3 class="text-center">LATEST ARTICLES</h3>

cca052df70f5c6ffbaa88e94c27343b109cfd237
nnorthcott <34338972@student.murdoch.edu.au>
2024-03-23 11:07:09 +0800

    Week 4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <h3 class="text-center">LATEST ARTICLES</h3>
                 <div class="row"><!--  a row that gives us access to the BS columns-->
                   <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port1.jpg">
+                      <img class="img-fluid" alt=“portait” src="http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port1.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -15,7 +15,7 @@
                         <p class="readmore">CONTINUE READING</p>
                     </div>
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port2.jpg">
+                      <img class="img-fluid" alt=“portait” src="http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port2.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -24,7 +24,7 @@
                     </div>
     
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port3.jpg">
+                      <img class="img-fluid" alt=“portait” src="http://170.187.231.66/~mesh15/wp-content/themes/starter_for_josh_site/images/port3.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
--- page_header.php
+++ page_header.php
@@ -4,14 +4,13 @@
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
 
-<!--<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="responsive.css" />-->
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css">
 <title>   <?php
               if (function_exists('is_tag') && is_tag()) {
                  single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
